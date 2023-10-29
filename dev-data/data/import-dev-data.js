3d4b4d2e16adef6747783ce0ac8c8c4a3a252037
manish mandal <76776194mm@gmail.com>
2023-10-30 00:38:53 +0530

    importing json data to DB

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const Tour = require('./../../models/tourModel.js');
+
+dotenv.config({ path: './config.env' });
+// this command will read the env variables from the confg file and save them into NODE JS environment variables
+// this is before `require app` because so that our environament variables will be read from the config file
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+// CONNECTING WITH HOSTED DATABASE
+mongoose.connect(DB, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}).then(() => {
+    console.log('DB connection successfull...');
+}).catch(err => {
+    console.log(err);
+});
+
+//READ JSON File
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'));
+
+const importData = async () => {
+    try {
+        await Tour.create(tours);
+        console.log('Data loaded successfully!..');
+    } catch (err) {
+        console.log(err);
+    }
+    process.exit();
+};
+
+const deleteData = async () => {
+    try {
+        await Tour.deleteMany();
+        console.log('Data successfully deleted! ');
+        //aggressive way of stopping application
+    } catch (err) {
+        console.log(err);
+    }
+    process.exit();
+}
+
+if (process.argv[2] === '--import') {
+    importData();
+} else if (process.argv[2] === '--delete') {
+    deleteData();
+}
+
+console.log(process.argv);
