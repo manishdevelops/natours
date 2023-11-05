b96686d670bb9c6c552edfe74fc9da8a99eafb25
manish mandal <76776194mm@gmail.com>
2023-11-05 14:41:39 +0530

    created route for monthly plan tour

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -11,6 +11,7 @@ const router = express.Router();
 
 router.route('/top-5-cheap').get(tourController.aliasTopTours, tourController.getAllTours);
 router.route('/tour-stats').get(tourController.getTourStats);
+router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan); // /:year -> url parameter
 router.route('/').get(tourController.getAllTours).post(tourController.createTour);
 router.route('/:id').get(tourController.getTour).patch(tourController.updateTour).delete(tourController.deleteTour);
 
