a46613335b266d9b15ca82267e471e13fff2941c
manish mandal <76776194mm@gmail.com>
2023-11-08 00:01:24 +0530

    implemented built-in validators for mongoose data model

--- models/tourModel.js
+++ models/tourModel.js
@@ -7,8 +7,11 @@ const tourSchema = new mongoose.Schema({
     name: {
         type: String,
         //required is validator
-        required: [true, 'A tour must have a name'],
-        unique: true
+        required: [true, 'A tour must have a name'], //a validator
+        unique: true, // not a validator
+        trim: true,
+        maxLength: [40, 'A tour name must have less or equal than 40 characters'],
+        minLength: [10, 'A tour name must have more or equal than 10 characters']
     },
     slug: String,
     duration: {
@@ -21,11 +24,17 @@ const tourSchema = new mongoose.Schema({
     },
     difficulty: {
         type: String,
-        required: [true, 'A tour must have a difficulty']
+        required: [true, 'A tour must have a difficulty'],
+        enum: {  // only for strings
+            values: ['easy', 'medium', 'difficulty'], // this should be the values 
+            message: 'Difficulty is either: easy, medium, difficult'
+        }
     },
     ratingsAverage: {
         type: Number,
-        default: 4.5
+        default: 4.5,
+        min: [1, 'Ratings must be above or equal 1.0'],
+        max: [5, 'Ratings must be below or equal to 5']
     },
     ratingsQuantity: {
         type: Number,
