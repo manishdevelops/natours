b40ffa6724bac6a8bbd1a2e9ae4640678d674fa5
manish mandal <76776194mm@gmail.com>
2023-11-13 21:04:46 +0530

    update handling unhandled routes

--- app.js
+++ app.js
@@ -70,27 +70,14 @@ app.use('/api/v1/users', userRouter);
 
 
 // If we are able to reach this point here it means that the request req res cycle was not yet finished at this point in our code 
-// app.use('*', (req, res, next) => { //the routes that are not handled by the above routes
-//     // * -> stands for all methods like get, post, put, patch etc
-//     res.status(404).json({
-//         status: 'fail',
-//         message: `Can't find ${req.originalUrl} on this server!`
-//         //req.original -> pathname that was requested
-//     });
-// });
-app.use((req, res, next) => {
-    const err = new Error(`Not Found - ${req.originalUrl}`);
-    err.status = 404;
-    next(err);
-});
-
-// Error handling middleware
-app.use((err, req, res, next) => {
-    res.status(err.status || 500).json({
-        error: {
-            message: err.message,
-        },
+app.use('*', (req, res, next) => { //the routes that are not handled by the above routes
+    // * -> stands for all methods like get, post, put, patch etc
+    res.status(404).json({
+        status: 'fail',
+        message: `Can't find ${req.originalUrl} on this server!`
+        //req.original -> pathname that was requested
     });
 });
+
 
 module.exports = app;
