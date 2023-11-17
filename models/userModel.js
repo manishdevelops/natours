cedf5674280b5b53391362457e1df0cd648004ec
manish mandal <76776194mm@gmail.com>
2023-11-17 11:58:46 +0530

    encypting user password

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -8,7 +9,7 @@ const userSchema = new mongoose.Schema({
     },
     email: {
         type: String,
-        required: [, 'Please provide your email'],
+        required: [true, 'Please provide your email'],
         unique: true,
         lowercase: true,  // converts to lowercase
         validate: [validator.isEmail, 'Please provide a valid email']
@@ -22,10 +23,27 @@ const userSchema = new mongoose.Schema({
     },
     passwordConfirm: {
         type: String,
-        required: [true, 'Please confirm your password']
+        required: [true, 'Please confirm your password'],
+        validate: {
+            // This only works on save
+            validator: function (el) {
+                return el === this.password;
+            },
+            message: 'Passwords are not same!'
+        }
     }
 });
 
+// executes between gatting the data and saving it to DB. PERFECT TIME TO MANIPULATE DATA
+// this middleware runs when there is modification and creation of the password
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 12);
+    this.passwordConfirm = undefined; // deleting the confirmPassword field because we need it only for validation although it was required field but it doesn't mean to be in the database 
+    next();
+})
+
 const User = mongoose.model('User', userSchema);
+// in DB -> By default, the collection name will be the lowercased and pluralized version of the model name
 
 module.exports = User;
