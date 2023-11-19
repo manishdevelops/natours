387ae048c923c6896b25dacbf90645d33e825bb9
manish mandal <76776194mm@gmail.com>
2023-11-19 12:14:36 +0530

    created route for user login

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,6 +5,8 @@ const authController = require('./../controllers/authController');
 const router = express.Router();
 
 router.post('/signup', authController.signup); // we need route for signup for only post data
+router.post('/login', authController.login);
+
 router.route('/').get(userController.getAllUsers).post(userController.createUser);
 router.route('/:id').get(userController.getUser).patch(userController.updateUser).delete(userController.deleteUser);
 
