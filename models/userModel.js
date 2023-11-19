c9c218fd128388d72e19d58157a3d02977d09c70
manish mandal <76776194mm@gmail.com>
2023-11-19 12:13:48 +0530

    added instance method for documents

--- models/userModel.js
+++ models/userModel.js
@@ -19,7 +19,8 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: [true, 'Please provide your password'],
-        minlength: 8
+        minlength: 8,
+        select: false
     },
     passwordConfirm: {
         type: String,
@@ -34,14 +35,19 @@ const userSchema = new mongoose.Schema({
     }
 });
 
-// executes between gatting the data and saving it to DB. PERFECT TIME TO MANIPULATE DATA
+// executes between getting the data and saving it to DB. PERFECT TIME TO MANIPULATE DATA
 // this middleware runs when there is modification and creation of the password
 userSchema.pre('save', async function (next) {
     if (!this.isModified('password')) return next();
     this.password = await bcrypt.hash(this.password, 12);
     this.passwordConfirm = undefined; // deleting the confirmPassword field because we need it only for validation although it was required field but it doesn't mean to be in the database 
     next();
-})
+});
+
+// this is the instance method that is gonna be available to all the documents of a cretain collection
+userSchema.methods.correctPassword = async function (candidatePassword, userPassword) {
+    return await bcrypt.compare(candidatePassword, userPassword); // return true if matches otherwise false
+}
 
 const User = mongoose.model('User', userSchema);
 // in DB -> By default, the collection name will be the lowercased and pluralized version of the model name
