5d6145930c73a7a7ab311ae50761853fb5af6538
manish mandal <76776194mm@gmail.com>
2023-11-19 12:15:26 +0530

    implement getting all users

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,18 @@
+const User = require('./../models/userModel');
+const catchAsync = require('./../utils/catchAsync');
 
-exports.getAllUsers = (req, res) => {
-    //500 -> internal server error
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet defined!'
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+    const users = await User.find();
+
+    //send response
+    res.status(200).json({
+        status: 'success',
+        results: users.length,
+        data: {
+            users
+        }
     });
-};
+});
 
 exports.getUser = (req, res) => {
     res.status(500).json({
