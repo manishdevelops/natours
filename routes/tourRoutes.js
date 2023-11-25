da9dee04c7f41ee8ef654f60490b8d2e348bb07b
manish mandal <76776194mm@gmail.com>
2023-11-25 13:13:34 +0530

    implemented middlewares

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const tourController = require('./../controllers/tourController');
+const authController = require('./../controllers/authController');
+
 const router = express.Router();
 
 // router middleware -> excutes in order they are written 
@@ -12,7 +14,7 @@ const router = express.Router();
 router.route('/top-5-cheap').get(tourController.aliasTopTours, tourController.getAllTours);
 router.route('/tour-stats').get(tourController.getTourStats);
 router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan); // /:year -> url parameter
-router.route('/').get(tourController.getAllTours).post(tourController.createTour);
-router.route('/:id').get(tourController.getTour).patch(tourController.updateTour).delete(tourController.deleteTour);
+router.route('/').get(authController.protect, tourController.getAllTours).post(tourController.createTour);
+router.route('/:id').get(tourController.getTour).patch(tourController.updateTour).delete(authController.protect, authController.restrictTo('admin', 'lead-guide'), tourController.deleteTour);
 
 module.exports = router;
