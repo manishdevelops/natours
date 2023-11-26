dc14531581d864fe46ab87893d118489bc5e4dcd
manish mandal <76776194mm@gmail.com>
2023-11-26 15:21:05 +0530

    implemented route for password forgot password

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,6 +7,9 @@ const router = express.Router();
 router.post('/signup', authController.signup); // we need route for signup for only post data
 router.post('/login', authController.login);
 
+router.post('/forgotPassword', authController.forgotPassword); //receive email for reset password
+router.patch('/resetPassword/:token', authController.resetPassword); //receive token as well as new password
+
 router.route('/').get(userController.getAllUsers).post(userController.createUser);
 router.route('/:id').get(userController.getUser).patch(userController.updateUser).delete(userController.deleteUser);
 
