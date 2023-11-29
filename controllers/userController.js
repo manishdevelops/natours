7ba605e9f5d69e6e01ab41d8d7f20c54bc7baa79
manish mandal <76776194mm@gmail.com>
2023-11-29 19:50:46 +0530

    updating current user data

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,14 @@
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
+
+const filterObj = (obj, ...allowedFields) => {
+    const newObj = {};
+    Object.keys(obj).forEach(el => {
+        if (allowedFields.includes(el)) newObj[el] = obj[el];
+    });
+    return newObj;
+}
 
 exports.getAllUsers = catchAsync(async (req, res, next) => {
     const users = await User.find();
@@ -10,6 +19,32 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
         results: users.length,
         data: {
             users
+        }
+    });
+});
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+    // 1)Create error if user POSTs password data
+    if (req.body.password || req.body.passwordConfirm) {
+        return next(new AppError('This route is not for password updates. Please use /updateMyPassword', 400));
+    }
+    // 2)Update user document
+
+    // const user = await User.findById(req.user.id);
+    // user.name = 'Mani';
+    // await user.save();  // There will be error because passwordConfirm is a required field and we havn't provided passwordConfirm and validators and middlewares run on `save` so we will use findByIdAndUpdate
+
+    const filterBody = filterObj(req.body, 'name', 'email'); // allowing only for name and email modification
+    const updatedUser = await User.findByIdAndUpdate(req.user.id, filterBody, {
+        new: true, //ensures that the method returns the modified user rather than the original one.
+        runValidators: true // instructs Mongoose to run validation on the fields specified in the update operation, since filterBody only contains the 'name' and 'email' fields, the validators will only run for those fields.
+    });
+
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            user: updatedUser
         }
     });
 });
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,8 +7,10 @@ const router = express.Router();
 router.post('/signup', authController.signup); // we need route for signup for only post data
 router.post('/login', authController.login);
 
-router.post('/forgotPassword', authController.forgotPassword); //receive email for reset password
-router.patch('/resetPassword/:token', authController.resetPassword); //receive token as well as new password
+router.post('/forgotPassword', authController.forgotPassword); //sends email for reset password
+router.patch('/resetPassword/:token', authController.resetPassword); //receive token via email as well as set new password
+router.patch('/updatePassword', authController.protect, authController.updatePassword); //only works for logged in users
+router.patch('/updateMe', authController.protect, userController.updateMe); // update user data of currently logged in user
 
 router.route('/').get(userController.getAllUsers).post(userController.createUser);
 router.route('/:id').get(userController.getUser).patch(userController.updateUser).delete(userController.deleteUser);
