42955931e78835b560a66ffba3acb013bdebafc4
manish mandal <76776194mm@gmail.com>
2023-12-24 18:52:38 +0530

    Modelling locations geospatial data

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -20,7 +20,7 @@ mongoose.connect(DB, {
 });
 
 //READ JSON File
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'));
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
 
 const importData = async () => {
     try {
