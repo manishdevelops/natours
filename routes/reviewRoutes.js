248fa62ad4da3ad5fa0ca549a767552ecb47f97a
manish mandal <76776194mm@gmail.com>
2023-12-27 22:46:53 +0530

    implemented nested routing with express

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -2,7 +2,8 @@ const express = require('express');
 const reviewController = require('./../controllers/reviewController');
 const authController = require('./../controllers/authController');
 
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
+// mergeParams: true  --> it means that the child router should inherit and merge the route parameters from its parent router and now can access parent parameters. This is done bxz each router has access to the parameters of their specific routes
 
 router.route('/').get(reviewController.getAllReviews).post(authController.protect, authController.restrictTo('user'), reviewController.createReview);
 
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const tourController = require('./../controllers/tourController');
 const authController = require('./../controllers/authController');
+const reviewRouter = require('./../routes/reviewRoutes');
 
 const router = express.Router();
+
+// POST/tours/1312hdjhs/reviews
+// GET/tours/1312hdjhs/reviews
+// GET/tours/1312hdjhs/reviews/4376643dafg
+// router.route('/:tourId/reviews').post(authController.protect, authController.restrictTo('user'), reviewController.createReview);
+
+// for this specific route we want to use the reviewRouter
+router.use('/:tourId/reviews', reviewRouter); // router is a middleware so we use the use method on it.
 
 // router middleware -> excutes in order they are written 
 
@@ -16,5 +25,7 @@ router.route('/tour-stats').get(tourController.getTourStats);
 router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan); // /:year -> url parameter
 router.route('/').get(authController.protect, tourController.getAllTours).post(tourController.createTour);
 router.route('/:id').get(tourController.getTour).patch(tourController.updateTour).delete(authController.protect, authController.restrictTo('admin', 'lead-guide'), tourController.deleteTour);
+
+
 
 module.exports = router;
