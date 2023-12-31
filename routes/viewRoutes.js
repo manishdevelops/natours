2f0aa389fd5792131c80da3843de2640bf1b21b6
manish mandal <76776194mm@gmail.com>
2023-12-31 15:39:07 +0530

    added login and map function

---
+++ routes/viewRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const viewsController = require('../controllers/viewController');
+
+const router = express.Router();
+
+
+// RENDERING TEMPLATES
+
+router.get('/', viewsController.getOverview);
+router.get('/tour/:slug', viewsController.getTour);
+router.get('/login', viewsController.getLoginForm);
+
+module.exports = router;
