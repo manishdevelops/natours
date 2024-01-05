dc73bf8be330a1244a69749f96b2df36dc7581d6
manish mandal <76776194mm@gmail.com>
2024-01-05 23:24:59 +0530

    implemented tour images upload functionality

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -32,6 +32,8 @@ router.route('/distances/:latlng/unit/:unit').get(tourController.getDistances);
 
 router.route('/').get(tourController.getAllTours).post(authController.protect, authController.restrictTo('admin', 'lead-guide'), tourController.createTour);
 
-router.route('/:id').get(tourController.getTour).patch(authController.protect, authController.restrictTo('admin', 'lead-guide'), tourController.updateTour).delete(authController.protect, authController.restrictTo('admin', 'lead-guide'), tourController.deleteTour);
+router.route('/:id').get(tourController.getTour)
+    .patch(authController.protect, authController.restrictTo('admin', 'lead-guide'), tourController.upploadTourImages, tourController.resizeTourImages, tourController.updateTour)
+    .delete(authController.protect, authController.restrictTo('admin', 'lead-guide'), tourController.deleteTour);
 
 module.exports = router;
